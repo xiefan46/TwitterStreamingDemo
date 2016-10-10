2b5a562688e4b9ebbaee35ea24d4ed1931d9f74c
xiefan46 <fan.xie@kyligence.io>
2016-10-10 18:45:01 +0800

    connect to kylin jdbc

---
+++ src/main/java/configuration/KylinJdbcSettings.java
@@ -0,0 +1,10 @@
+package configuration;
+
+/**
+ * Created by xiefan on 16-10-10.
+ */
+public interface KylinJdbcSettings {
+    String USERNAME="fan.xie";
+    String PASSWORD="mypassword";
+    String JDBC_URL="jdbc:kylin://52.33.52.22:7070/streaming";
+}
